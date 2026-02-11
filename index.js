010fb9d60cf81e160f12bb00a0fe93d530139afa
Aditya Kiran Shukla <adityashukla600700@gmail.com>
2026-02-11 11:21:54 +0530

    Thsi is the one main

---
+++ index.js
@@ -0,0 +1,18 @@
+// console.log(`Hello`);
+// console.log(`I like you`);
+
+// window.alert(`This is an alert`);
+
+// document.getElementById("asdf").textContent = "Hello";
+// document.getElementById("myp").textContent = "I like you";
+
+let age=21;
+let price = 100.00;
+let cgpa = 8.0;
+
+console.log(typeof cgpa);
+
+console.log(`You are ${age} years old`);
+console.log(`The price is Rs${price}`);
+console.log(`Your cgpa is: ${cgpa}`);
+
